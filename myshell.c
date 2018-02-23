2fbc1ca6966c5d0b039dd809898c2b65bd9c1030
Matthew Chiang <matthewchiang18@gmail.com>
2018-02-23 18:25:20 -0500

    Semicolon and Ampersand Work
    Check for bugs later

--- myshell.c
+++ myshell.c
@@ -1,4 +1,4 @@
-// Ashley Yiu
+// Matthew Chiang and Ashley Yiu
 // OS Assignment 1
 // 2/28/2018
 
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <regex.h>
+#include <sys/wait.h>
 
 #define BUFF_SIZE 2048
 #define MAX_HISTORY_CMDS 5
@@ -21,10 +22,11 @@ void clearHistory();
 void createPipeWrapper(char* input);
 void createPipe(char* cmd1, char* cmd2);
 void skipBlanks(char* inputStr);
-int runCommand(char*);
+int runCommand(char*, int);
 
 const int PIPE_READ = 0;
 const int PIPE_WRITE = 1;
+char* HOME_DIREC;
 int fd[2];
 int HISTORY_START = 0;
 int HISTORY_INDEX = 0; // points to next avail space
@@ -32,12 +34,21 @@ char* HISTORY_CMDS[MAX_HISTORY_CMDS][MAX_HISTORY_CMDS];
 
 int main( int argc, char *argv[] )  {
 	char buffer[BUFF_SIZE];
+	// const char homeDirec[BUFF_SIZE];
+	// getcwd(homeDirec, BUFF_SIZE);
+	// HOME_DIREC = &homeDirec;
+	getcwd(HOME_DIREC, BUFF_SIZE);
+
 	printf("$");
-    while (fgets (buffer, BUFF_SIZE, stdin))
-    {
-    	if (strcmp(buffer, "exit")==0) {
-    		exit(0);
-    	}
+	while (fgets (buffer, BUFF_SIZE, stdin))
+	{
+		if (strcmp(buffer, "exit\n")==0) {
+			exit(0);
+		}
+		if (!strcmp(buffer, "\n") || !strcmp(buffer, "\r\n")) { //empty line
+			printf("$");
+			continue;
+		}
 		remove_trailing_newline_char(buffer);
 		skipBlanks(buffer);
 		printf("your input: %s\n", buffer);
@@ -50,9 +61,9 @@ int main( int argc, char *argv[] )  {
 		store_in_history(buffer);
 		printf("Now we are about to process line #%d\n", HISTORY_INDEX);
 		printf("$");
-    }
+	}
 
-    return 0;
+	return 0;
 }
 
 // removes trailing \n inline
@@ -93,30 +104,95 @@ void store_in_history(char* buffer)
 int tokenize(char *input) {
 	regex_t regex;
 	char* offset;
-	char* semicolon = strchr(input, ';');
+	char* semicolon = NULL;
+	char* ampersandLoc = NULL;
 	int offsetInt;
 	int rv;
 	int i = 0;
 	int retVal;
+	char inputCopy[BUFF_SIZE];
+	char input_to_tokens[BUFF_SIZE];
 
-	skipBlanks(input);
-	printf("Now tokenizing input: %s...\n", input);
-	if (strncmp(input, "cd ", 3) == 0) {
+	strcpy(inputCopy, input); // dest, source
+	strcpy(input_to_tokens, input); // dest, source
+	char* command = strtok(input_to_tokens, " "); // first argument of input
+
+	skipBlanks(inputCopy);
+
+	// check for multiple commands
+	semicolon = strchr(inputCopy, ';');
+	if (semicolon) {
+		char* charIter = inputCopy;
+		while (semicolon != NULL) {
+			char cur_command[256] = {0};
+			for (; charIter < semicolon; charIter++) {
+				// printf("letters before semicolon: %c\n", *charIter);
+				int cur_len = strlen(cur_command);
+				cur_command[cur_len] = *charIter;
+				cur_command[cur_len+1] = '\0';
+			}
+			// printf("current command: %s\n", cur_command);
+
+			// run command
+			retVal = tokenize(cur_command);
+			semicolon = strchr(semicolon+1, ';'); // if valid, continue w/ next command
+			charIter++; //skip ; char
+		}
+		// printf("at end\n");
+		char* endOfInput = strchr(inputCopy, '\0');
+		char cur_command[256] = {0};
+		for (; charIter < endOfInput; charIter++) {
+			// printf("letters before end: %c\n", *charIter);
+			int cur_len = strlen(cur_command);
+			cur_command[cur_len] = *charIter;
+			cur_command[cur_len+1] = '\0';
+		}
+		// printf("last command: %s\n", cur_command);
+		retVal = tokenize(cur_command);
+		return retVal;
+	}
+
+	// check for ampersand
+	ampersandLoc = strchr(inputCopy, '&');
+	if (ampersandLoc) {
+		// printf("has ampersand: tokenize\n");
+		memmove(ampersandLoc, ampersandLoc+1, strlen(inputCopy)); //remove & from input
+	}
+
+	// DEBUG: Print the actual command to run
+	printf("Now tokenizing input: \"%s\"\n", inputCopy);
+
+	// cd
+	if (!strcmp(command, "cd")) {
 		printf("is cd\n");
-		changeDirectory(input+3);
-	} 
-	else if (strcmp(input, "history") == 0) {
-		store_in_history(input);
+		char* destination = strtok(NULL, " ");
+
+		//no destination: go home
+		if (destination == NULL) {
+			if (HOME_DIREC == NULL) {
+				printf("no dest or home direc\n");
+			}
+			changeDirectory(HOME_DIREC);
+		}
+		// destination exists
+		else { 
+			changeDirectory(destination);
+		}
+		return 1; //write in history
+	} //end cd
+	// history
+	else if (strcmp(inputCopy, "history") == 0) {
+		store_in_history(inputCopy);
 		showHistory();
 		return 1;
-	} 
-	else if (strcmp(input, "history -c") == 0) {
+	}
+	else if (strcmp(inputCopy, "history -c") == 0) {
 		printf("clear history\n");
 		clearHistory();
 		return 1;
 	} 
-	else if (strncmp(input, "history ", 8) == 0) {
-		offset = (input+8);
+	else if (strncmp(inputCopy, "history ", 8) == 0) {
+		offset = (inputCopy+8);
 		printf("offset: '%s'\n", offset);
 		rv = regcomp(&regex, "[[:digit:]]{1,2}", REG_EXTENDED);
 		if (rv != 0) {
@@ -141,53 +217,15 @@ int tokenize(char *input) {
 				printf("no offset match\n");
 			}
 		}
-	}
-	//not cd or history: run command
-	else if (semicolon != NULL) {
-		char* charIter = input;
-		while (semicolon != NULL) {
-			// printf("has semicolon\n");
-			char cur_command[256] = {0};
-			for (; charIter < semicolon; charIter++) {
-				// printf("letters before semicolon: %c\n", *charIter);
-				int cur_len = strlen(cur_command);
-				// printf("%i\n", cur_len);
-				cur_command[cur_len] = *charIter;
-				cur_command[cur_len+1] = '\0';
-			}
-			// printf("current command: %s\n", cur_command);
+	} //end history
 
-			// run command
-			retVal = tokenize(cur_command);
-			if (retVal) return retVal; // if invalid, exit
-			semicolon = strchr(semicolon+1, ';'); // if valid, continue w/ next command
-			charIter++; //skip ; char
-		}
-		// printf("at end\n");
-		char* endOfInput = strchr(input, '\0');
-		char cur_command[256] = {0};
-		for (; charIter < endOfInput; charIter++) {
-			// printf("letters before end: %c\n", *charIter);
-			int cur_len = strlen(cur_command);
-			// printf("%i\n", cur_len);
-			cur_command[cur_len] = *charIter;
-			cur_command[cur_len+1] = '\0';
-		}
-		// printf("last command: %s\n", cur_command);
-		retVal = tokenize(cur_command);
+	//not cd or history: run command
+	else {
+		retVal = runCommand(inputCopy, (ampersandLoc == NULL) ? 0 : 1);
 		return retVal;
 	}
-	//not cd or history: run command
-	else { // run built-in executables
-		// if (semicolon != NULL) {
-			
-		// }
-		// else {
-			retVal = runCommand(input);
-			return retVal;
-		// }
-	}
-	return 0; //success
+
+	return -1;
 }
 	//you will have to handle (; & < >) characters.
 
@@ -299,14 +337,14 @@ void changeDirectory(char *newDirectory)
 {
 	printf("Changing Directory to: '%s'\n", newDirectory);
 	char cwd[1024];
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-	   printf("Current Directory is: %s\n", cwd);
+	if (getcwd(cwd, sizeof(cwd))) {
+		printf("Current Directory is: %s\n", cwd);
 	}
-	if (chdir(newDirectory) == 0) {
-		if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		   printf("New Directory is now: %s\n", cwd);
+	if (!chdir(newDirectory)) {
+		if (getcwd(cwd, sizeof(cwd))) {
+			printf("New Directory is now: %s\n", cwd);
 		} else {
-		   perror("getcwd() error");
+			perror("getcwd() error");
 		}
 	} else {
 		perror("Failed to change directory");
@@ -314,7 +352,9 @@ void changeDirectory(char *newDirectory)
 
 }
 
-int runCommand(char* input) {
+int runCommand(char* input, int hasAmpersand) {
+	printf("Here is input: %s\n", input);
+	printf("Here is hasAmpersand: %i\n", hasAmpersand);
 	// createPipeWrapper(input);
 	int argNum=0;
 	char* shellArgs[BUFF_SIZE];
@@ -327,27 +367,35 @@ int runCommand(char* input) {
 		shellArgs[++argNum] = strtok(NULL," "); //parse any more args
 	}
 	printf("Num of args: %i\n", argNum);
-	printf("parsed arg:\n");
+	printf("Listing all arguments:\n");
 	for (int index=0;index<argNum; index++)
 	{
-		printf("%s\n", shellArgs[index]);
+		printf("[%i] %s\n", index, shellArgs[index]);
 	}
 
 	strcpy(command, "/bin/");
 	strcat(command, shellArgs[0]);
-	int rc = fork();
-	if (rc < 0) { // fork failed
+	int pid = fork();
+	if (pid < 0) { // fork failed
 		printf("Fork failed\n");
 		return -1;
 	}
-	else if (rc == 0) { // child runs command
+	else if (pid == 0) { // child runs command
 		printf("Fork success\n");
 
 		execv(command, shellArgs);
-		printf("After fork\n");
+		printf("Exec failed\n");
 	}
-	else { // parent: wait until child is done
-		wait();
+	else { // parent: wait until child is done (unless &)
+		if (!hasAmpersand) {
+			waitpid(pid, NULL, 0);
+			printf("after wait\n");
+		}
+		else {
+			return 0;
+			printf("has amp\n");
+		}
+
 	}
 	return 0;
 }
